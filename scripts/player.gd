f54f959d32ee059b9e7d1e9fe276eb6d60e89b05
catarinagc <catarinagcostaa@gmail.com>
2025-03-29 18:49:56 +0000

    started tower

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,15 +1,25 @@
 extends CharacterBody2D
 
-
+const TOWER = preload("res://scenes/tower.tscn")
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 var amount = 0
+var towersAvailable = 5
 
 func _physics_process(delta: float) -> void:
 	var input_dir = Input.get_vector("left","right","up","down")	
 	velocity = input_dir * SPEED
 
 	move_and_slide()
+	
+	if (Input.is_action_just_pressed("build")):
+		if towersAvailable > 0:
+			var newTower = TOWER.instantiate()
+			newTower.global_position = global_position
+			get_tree().root.add_child(newTower)
+			towersAvailable -= 1
+		else:
+			print("no build")
 
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
