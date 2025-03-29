ddc2c2fbe24142c074afca56eb7c2dcafb05744f
Sara Marques <saramarquesgamedev@gmail.com>
2025-03-29 21:12:33 +0000

    Adjustments to credits

--- global.gd
+++ global.gd
@@ -2,15 +2,10 @@ extends Node
 
 var currentResolution = Vector2i(1152,648)
 
-var lastHighScore = 0;
+var highScore = 0;
 var score = 0
 var windowMode = 1
 var selectedResolution = 0
-
-var SCALE_MAX_WEIGHT : int = 75;
-
-var highest_left_block_position = -INF;
-var highest_right_block_position = -INF;
 
 var isFullscreen = true
 
--- scenes/menus/credits_menu.gd
+++ scenes/menus/credits_menu.gd
@@ -1,8 +1,16 @@
 extends Control
 
+func seconds2mmss(total_seconds: float) -> String:
+	#total_seconds = 12345
+	var seconds:float = fmod(total_seconds , 60.0)
+	var minutes:int   =  int(total_seconds / 60.0) % 60
+	var mmss_string:String = "%02d:%05.2f" % [minutes, seconds]
+	return mmss_string
+	
 func _ready() -> void:
-	$MarginContainer/VBoxContainer/Label.text = "Score: " + str(Global.score)
-
+	$MarginContainer/VBoxContainer/Label.text = "Time Survived: " + seconds2mmss(Global.score);
+	$MarginContainer/VBoxContainer/Label3.text = "Longest Record: " + seconds2mmss(Global.highScore);
+	
 func _on_back_to_menu_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/menus/main_menu.tscn")
 
