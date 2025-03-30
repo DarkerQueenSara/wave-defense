c8194ffbeaf0a29829fbe0d64c33e93685b4bf26
catarinagc <catarinagcostaa@gmail.com>
2025-03-30 15:13:07 +0100

    fix bug

--- scripts/repel.gd
+++ scripts/repel.gd
@@ -1,9 +1,11 @@
 extends TurretBase
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	super(body);
-	body.currentLookAt = body.get_parent().spawnPointPos;
+	if (body.is_in_group("tower")):
+		super(body);
+		body.currentLookAt = body.get_parent().spawnPointPos;
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
-	super(body);
-	body.currentLookAt = Vector2(0,0);
+	if (body.is_in_group("tower")):
+		super(body);
+		body.currentLookAt = Vector2(0,0);
