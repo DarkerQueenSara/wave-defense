baf4a05253c10d26924a3068b08ff43631ea4aa2
catarinagc <catarinagcostaa@gmail.com>
2025-03-30 15:17:48 +0100

    bug fix

--- scripts/magnet.gd
+++ scripts/magnet.gd
@@ -1,12 +1,12 @@
 extends TurretBase
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if (body.is_in_group("tower")):
+	if (body.is_in_group("enemies")):
 		print("here")
 		super(body);
 		body.currentLookAt = self.global_position;
 #
 func _on_area_2d_body_exited(body: Node2D) -> void:
-	if (body.is_in_group("tower")):
+	if (body.is_in_group("enemies")):
 		super(body);
 		body.currentLookAt = Vector2(0,0);
--- scripts/repel.gd
+++ scripts/repel.gd
@@ -1,11 +1,11 @@
 extends TurretBase
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if (body.is_in_group("tower")):
+	if (body.is_in_group("enemies")):
 		super(body);
-		body.currentLookAt = body.get_parent().spawnPointPos;
+		body.currentLookAt = body.spawnPointPos;
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
-	if (body.is_in_group("tower")):
+	if (body.is_in_group("enemies")):
 		super(body);
 		body.currentLookAt = Vector2(0,0);
