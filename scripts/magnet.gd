a0ae08b00b9ba5851e49b70d91c8e92ea526b965
catarinagc <catarinagcostaa@gmail.com>
2025-03-30 15:10:23 +0100

    turret fix

--- scripts/magnet.gd
+++ scripts/magnet.gd
@@ -3,8 +3,8 @@ extends TurretBase
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	print("here")
 	super(body);
-	body.get_parent().currentLookAt = self.global_position;
+	body.currentLookAt = self.global_position;
 #
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	super(body);
-	body.get_parent().currentLookAt = Vector2(0,0);
+	body.currentLookAt = Vector2(0,0);
